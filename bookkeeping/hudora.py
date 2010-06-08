4afb9a3e201dda00ff85aa325d79d22c048a1600
Christian Klein <c.klein@hudora.de>
2010-06-08 11:58:31 +0200

    store_order und read_invoice implementiert - als modul-funktionen (nicht als instanz-methoden)

--- bookkeeping/hudora.py
+++ bookkeeping/hudora.py
@@ -7,23 +7,81 @@ Created by Maximillian Dornseif on 2010-06-04.
 Copyright (c) 2010 HUDORA. All rights reserved.
 """
 
+import datetime
+import husoftm.artikel
+import husoftm.auftraege
+import husoftm.kunden
+import husoftm.rechnungen
+import husoftm.sachbearbeiter
 import husoftm.stapelschnittstelle
+from decimal import Decimal
+
+ABSENDER_ADRESSE = u"HUDORA GmbH\nJägerwald 13\nD-42897 Remscheid"
 
 
-class HudoraBookkeeping(bookkeeping.abstract.AbstractBookkeeping):
+def store_order(order):
+    """
+    Schreibe einen Auftrag in die SoftM-Stapelschnittstelle.
     
-    def store_order(self, order):
-        """Auftrag in SoftM Stapelschnittstelle schreiben"""
-        vorgangsnr = husoftm.stapelschnittstelle.auftrag2softm(order)
-        if not husoftm.stapelschnittstelle.address_transmitted(vorgangsnr):
-            raise RuntimeError("Fehler bei Vorgang %s: Die Addresse wurde nicht korrekt uebermittelt." % vorgangsnr)
+    Es wird erwartet, dass der Auftrag dem ExtendedOrderProtocol entspricht.
+    """
+    vorgangsnr = husoftm.stapelschnittstelle.auftrag2softm(order)
+    if not husoftm.stapelschnittstelle.address_transmitted(vorgangsnr):
+        raise RuntimeError("Fehler bei Vorgang %s: Die Addresse wurde nicht korrekt uebermittelt." % vorgangsnr)
 
-    def check_order(order):
-        pass
+
+def list_invoices(kdnr):
+    """Erzeuge eine Liste mit allen Rechnungsnummern zu der gegebenen Kundennummer"""
+    return husoftm.rechnungen.rechnungen_for_kunde(kdnr)
+
+
+def read_invoice(kdnr, rechnungsnr):
+    """Erzeuge eine Rechnung im SimpleInvoiceProtocol aus der Rechnung mit der gegebenen Rechnungsnummer"""
+    
+    kunde = husoftm.kunden.get_kunde(kdnr)
+    rechnungskopf, rechnungspositionen = husoftm.rechnungen.get_rechnung(rechnungsnr)
+    auftragskopf, auftragspositionen = husoftm.auftraege.get_auftrag(rechnungskopf['auftragsnummer'])
+    
+    invoice = {
+        'kundennr': kdnr,
+        'name1': kunde.name1,
+        'name2': kunde.name2,
+        'strasse': kunde.strasse,
+        'ort': kunde.ort,
+        'plz': kunde.plz,
+        'land': kunde.land,
+        'tel': kunde.tel,
+        'mail': kunde.mail,
+        'iln': kunde.iln,
+        
+        'guid': None, # XXX
+        'leistungszeitpunkt': datetime.date.today().strftime('%Y-%m-%dT%H:%M:%S'),
+        'kundenauftragsnr': auftragskopf['auftragsnr_kunde'],
+        'infotext_kunde': "", # XXX
+        'versandkosten': int(400 / 1.19), # XXX,
+        'absenderadresse': ABSENDER_ADRESSE,
+        'erfasst_von': husoftm.sachbearbeiter.name(auftragskopf['sachbearbeiter']),
+        'preis': int(rechnungskopf['netto'] * 100),
+        'orderlines': [],
+    }
+    
+    orderlines = []
+    for index, position in enumerate(rechnungspositionen):
+        artikel = husoftm.artikel.get_artikel(artnr=position['artnr'])
+        invoice['orderlines'].append({'guid': '%s-%d' % (invoice['guid'], index),
+                                      'menge': position['menge'],
+                                      'artnr': position['artnr'],
+                                      'ean': artikel.ean,
+                                      'infotext_kunde': '',
+                                      'preis': int(position['wert_netto'] / Decimal("1.19")),
+                                     })
+    return invoice
 
 
 def main():
-    pass
+    invoice = read_invoice('66666', '1065659')
+    import pprint
+    pprint.pprint(invoice)
 
 
 if __name__ == '__main__':
