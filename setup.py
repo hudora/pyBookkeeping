97f395dbaf5affc88fd34f70018db59d32b5b2ad
Christian Klein <c.klein@hudora.de>
2010-06-09 13:43:03 +0200

    requirements angepasst und Fehler in setup.py korrigiert

--- setup.py
+++ setup.py
@@ -1,26 +1,21 @@
 #!/usr/bin/env python
 # encoding: utf-8
-"""%%PROJECTNAME%% is xXXXx
 """
-
-# setup.py
-# Created by Maximillian Dornseif on 2010-04-07 for HUDORA.
-# Copyright (c) 2010 HUDORA. All rights reserved.
+pyBookkeeping - Functionality related to bookkeeping
+"""
 
 from setuptools import setup, find_packages
 
-setup(name='%%PROJECTNAME%%',
+setup(name='pyBookkeeping',
       # maintainer='XXX',
       # maintainer_email='xXXXx@hudora.de',
-      version='1.0',
-      description='xXXXx FILL IN HERE xXXXx',
-      long_description=long_description=codecs.open('README.rst', "r", "utf-8").read(),
+      version='0.1',
+      description='Buchhaltungsfunktionen',
+      long_description=codecs.open('README.rst', "r", "utf-8").read(),
       classifiers=['License :: OSI Approved :: BSD License',
                    'Intended Audience :: Developers',
                    'Programming Language :: Python'],
       download_url='https://cybernetics.hudora.biz/nonpublic/eggs/',
-      package_data={"%%MODULENAME%%": ["templates/%%MODULENAME%%/*.html", "reports/*.jrxml", "bin/*"]},
       packages=find_packages(),
-      include_package_data=True,
-      install_requires=['huTools', 'huDjango'],
+      install_requires=['oauth2', 'cs>=0.11', 'tlslite', 'huSoftM'],
 )
